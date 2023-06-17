eb017bb53bb1625c9463eaaa9d7d247b56e88d94
patdixan <patdixan@sheridancollege.ca>
2023-06-16 20:54:34 -0700

    Draft 2

---
+++ src/ca/sheridancollege/project/WarGame.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author dixan
+ */
+import java.util.ArrayList;
+
+public class WarGame extends Game {
+
+    public WarGame(String name) {
+        super(name);
+    }
+
+    @Override
+    public void play() {
+
+        // Steps to consider:
+        // 1. Create a deck of cards
+        // 2. Shuffle the deck
+        // 3. Divide the deck between players
+        // 4. Start the game loop
+        // 5. Each player plays a card
+        // 6. Compare the ranks of the cards
+        // 7. Handle the case of a tie ("war")
+        // 8. Continue steps 5-7 until a player
+        // 9. Announce the winner
+    }
+
+    @Override
+    public void declareWinner() {
+        //Method to declare and display the winning player
+    }
+}
